6acfa25eb05ae195ee6c44035d26aff735085972
Jesse Chong <jec334@gmail.com>
2021-10-25 21:16:31 -0700

    feat: Printing out onto html

--- js/wsp.js
+++ js/wsp.js
@@ -1,7 +1,42 @@
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
 $(document).ready(function() {
-    $("#epicButton").click(function() {
-        $("#workRow").append("<p>Oh yeah</p>");
-        console.log("OH YEAH!");
+    $("#epicButton").click(async function() {
+        var wordOneValue = $("#word1").val();
+        var wordTwoValue = $("#word2").val();
+        var maximumLength = Math.max(wordOneValue.length, wordTwoValue.length);
+        var counter = 0;
+
+        for (let i = 0; i < maximumLength; i++) {
+            let w1v = wordOneValue[i];
+            let w2v = wordTwoValue[i];
+
+            if (w1v == w2v) {
+                counter++;
+            }
+
+            if (w1v != null) {
+                $("#workCol1").append("<p>" + w1v + "</p>");
+            } else {
+                $("#workCol1").append("<p>" + " " + "</p>");
+            }
+
+            if (w2v != null) {
+                $("#workCol2").append("<p>" + w2v + "</p>");
+            } else {
+                $("#workCol2").append("<p>" + " " + "</p>");
+            }
+
+            await sleep(100);
+        }
+
+        $("#resultValue").replaceWith("<span id=\"resultValue\">" + counter + "</span");
+
+        console.log(maximumLength);
+        console.log(wordOneValue);
+        console.log(wordTwoValue);
     })
 });
 
