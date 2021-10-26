d2a0156e432a208929dacd0ca2fad4612162be5a
Jesse Chong <jec334@gmail.com>
2021-10-25 20:51:14 -0700

    feat: Basic functionality defined

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # wackyStringComparer
+TODO: Some tool I made for fun. Will compare 2 strings and do some crazy frontend stuff.
---
+++ js/wsp.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+    $("#epicButton").click(function() {
+        $("#workRow").append("<p>Oh yeah</p>");
+        console.log("OH YEAH!");
+    })
+});
+
